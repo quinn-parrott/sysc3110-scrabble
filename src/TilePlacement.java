54ffd83a18f9ad8a0abd760f352d587a98ded8fa
mande <colinmandeville@cmail.carleton.ca>
2022-10-15 15:59:02 -0400

    Implements first version of TilePlacement class

--- src/TilePlacement.java
+++ src/TilePlacement.java
@@ -3,6 +3,16 @@ import java.util.ArrayList;
 public class TilePlacement {
     // Written by Colin Mandeville, 101140289
     // This represents a single player turn
-    private ArrayList<Tile> tiles;
-    private int score = 0;
+    private ArrayList<TilePositioned> tiles;
+    private int score;
+
+    public TilePlacement(ArrayList<TilePositioned> tiles) {
+        this.tiles = tiles;
+        for (TilePositioned tile : this.tiles) {
+
+            // TODO Implement putting words onto board, need to wait on CLI and Game being implemented
+
+            this.score += tile.getTile().getPointValue();
+        }
+    }
 }
--- src/TilePositioned.java
+++ src/TilePositioned.java
@@ -2,4 +2,17 @@ public class TilePositioned {
     // Written by Colin Mandeville, 101140289
     private Tile tile;
     private Position pos;
+
+    public TilePositioned(Tile tile, Position pos) {
+        this.tile = tile;
+        this.pos = pos;
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+
+    public Position getPos() {
+        return pos;
+    }
 }
