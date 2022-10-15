f9e51934a2c25c754a64b924180a8db2007467e4
mande <colinmandeville@cmail.carleton.ca>
2022-10-15 13:41:10 -0400

    Defined tile class

--- src/ca/carleton/sysc3110/group13/scrabble/Tile.java
+++ src/ca/carleton/sysc3110/group13/scrabble/Tile.java
@@ -1,6 +1,20 @@
 package ca.carleton.sysc3110.group13.scrabble;
 
 public class Tile {
-    // TODO: Colin
+
     private char chr;
+    private int pointValue;
+
+    public Tile(char chr, int pointValue) {
+        this.chr = chr;
+        this.pointValue = pointValue;
+    }
+
+    public char getChr() {
+        return chr;
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
 }
