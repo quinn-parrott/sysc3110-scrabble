54c038afd4e30fdb3393a4bac053aa7baafbf17f
Josaphat3 <lifulaj@gmail.com>
2022-10-16 14:17:47 -0400

    Implemented initial version of the player class

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,65 @@
 import java.util.ArrayList;
 
+/**
+ * @author Tao Lufula, 101164153
+ */
 public class Player {
-    // TODO: Tao
-    private ArrayList<Tile> currentTileHand;
+
+    private String name;
+    private int points;
+    private ArrayList<Tile> tileHand;
+    private static final int TILEHANDSIZE = 7;
+
+    /**
+     * Default constructor for player
+     * @param name the name of the player
+     * @author Tao Lufula, 101164153
+     */
+    public Player(String name) {
+        this.name = name;
+        this.points = 0;
+        this.tileHand = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public ArrayList<Tile> getTileHand() {
+        return tileHand;
+    }
+
+    /**
+     * This method adds the given points to the player's points
+     * @param points
+     * @author Tao Lufula, 101164153
+     */
+    public void addPoints(int points){
+        this.points += points;
+    }
+
+    /**
+     * This method add a tile to the players tileHand if the player's hand has less than 7 tiles.
+     * @param tile
+     * @author Tao Lufula, 101164153
+     */
+    public void addTile(Tile tile){
+        if(this.tileHand.size() < TILEHANDSIZE){
+            this.tileHand.add(tile);
+        }
+
+    }
+
+    /**
+     * This method remove a tile from the player's tile hand.
+     * @param tile
+     *  @author Tao Lufula, 101164153
+     */
+    public void removeTile(Tile tile){
+        this.tileHand.remove(tile);
+    }
 }
