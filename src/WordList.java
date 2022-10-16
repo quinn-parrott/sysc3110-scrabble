9ec19c2b0a3301739eb459cc34728a4ec5dee340
Josaphat3 <lifulaj@gmail.com>
2022-10-16 17:03:50 -0400

    Implemented initial version of the WordList Class

--- src/WordList.java
+++ src/WordList.java
@@ -1,5 +1,41 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.Scanner;
+
+/**
+ * @author Tao Lufula, 101164153
+ */
 
 public class WordList {
-    HashSet<String> wordlist;
+    private HashSet<String> wordlist;
+
+    /**
+     * Default constructor for WordList class
+     * @throws IOException
+     * @author Tao Lufula, 101164153
+     */
+    public WordList() throws IOException {
+        this.wordlist = new HashSet<>();
+        this.readWordListFile("wordList.txt");
+    }
+
+    /**
+     * This method reads words from a text file and add them to the HashSet WordList
+     * @param fileName the name of the txt file to be read
+     * @throws IOException
+     *
+     * @author Tao Lufula, 101164153
+     */
+    public void readWordListFile(String fileName) throws IOException {
+        Path path = Paths.get(fileName);
+        try (Scanner scanner =  new Scanner(path)){
+            while (scanner.hasNextLine()){
+                this.wordlist.add(scanner.nextLine());
+            }
+        }
+    }
 }
+
+
