2bc75ea0ad08e265b2fac9ea5a2cb89fe1cf67bd
Quinn Parrott <quinnparrott@cmail.carleton.ca>
2022-10-18 15:59:01 -0400

    Added way to check if tiles are filled with a letter

--- src/Tile.java
+++ src/Tile.java
@@ -2,4 +2,8 @@
  * @author Colin Mandeville, 101140289
  */
 public record Tile(char chr, int pointValue) {
+    public boolean isFilledWithLetter() {
+        // TODO: Make it impossible to have empty tiles
+        return chr != '_' && chr != '*';
+    }
 }
