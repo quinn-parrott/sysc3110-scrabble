07317321bdc1bddc11a1060e7a1a5060fca098ec
mande <colinmandeville@cmail.carleton.ca>
2022-10-24 21:48:00 -0300

    Board Javadocs

--- src/Board.java
+++ src/Board.java
@@ -28,44 +28,88 @@ public class Board {
         }
     }
 
+    /**
+     * Calculates the middle square of the board ArrayList
+     * @return int equal to the index of the center tile
+     * @author Colin Mandeville, 101140289
+     */
     public static int getCenterTilePos() {
         return Math.floorDiv(COLUMN_NUMBER * ROW_NUMBER, 2);
     }
-// TODO: CLEAN UP GETTERS AND SETTERS TO ALL USE POSITION
+
+    /**
+     * Sets a tile object to be contained at a specific pos
+     * @param tile tile to be set to the position
+     * @param pos pos where the tile will be set
+     * @author Quinn Parrott, 101169535
+     */
     public void setTile(Tile tile, Position pos) {
         setTile(tile, pos.getX(), pos.getY());
     }
 
+    /**
+     * Sets a tile object to be contained at a specific x and y coordinate, where y represents row and x represents column
+     * @param tile tile to be set to the x and y coordinate
+     * @param x x coordinate representing the column number
+     * @param y y coordinate representing the row number
+     * @author Quinn Parrott, 101169535
+     */
     public void setTile(Tile tile, int x, int y) {
         if (Position.FromInts(x, y).isPresent()) {
             setTile(tile, Position.FromInts(x, y).get().getIndex());
         }
     }
 
+    /**
+     * Sets a tile object to be contained at a specific index of the ArrayList
+     * @param tile tile to be set to a specific index of this.board
+     * @param index index of this.board
+     * @author Quinn Parrott, 101169535
+     */
     public void setTile(Tile tile, int index) {
         this.board.set(index, tile);
     }
 
+    /**
+     * Getter method for tiles contained at a set Position on the board
+     * @param pos pos is a Position object referencing an index on the board
+     * @return Returns the tile object at pos Position on the board
+     * @author Quinn Parrott, 101169535
+     */
     public Optional<Tile> getTile(Position pos) {
         return getTile(pos.getX(), pos.getY());
     }
 
+    /**
+     * Getter method for tiles contained at a set x,y coordinate on the board
+     * @param x x referring to column number
+     * @param y y referring to row number
+     * @return Returns the tile object at an x y coordinate on the board
+     * @author Quinn Parrott, 101169535
+     */
     public Optional<Tile> getTile(int x, int y) {
         Optional<Position> i = Position.FromInts(x, y);
         return i.map(position -> this.board.get(position.getIndex()));
     }
 
+    /**
+     * @return Returns the constant COLUMN_NUMBER
+     * @author Quinn Parrott, 101169535
+     */
     public static int getCOLUMN_NUMBER() {
         return COLUMN_NUMBER;
     }
 
+    /**
+     * @return Returns the constant ROW_NUMBER
+     * @author Quinn Parrott, 101169535
+     */
     public static int getROW_NUMBER() {
         return ROW_NUMBER;
     }
 
     /**
      * Place a TilePlacement on the board
-     *
      * @author Quinn Parrott, 101169535
      */
     public void placeTiles(TilePlacement tilePlacement) throws PlacementException {
@@ -78,6 +122,11 @@ public class Board {
             if (tileOpt.get().isFilledWithLetter()) {
                 throw new PlacementException(String.format("'%s' already has a letter", tile.pos()), tilePlacement, Optional.of(this));
             }
+            // TODO Make sure words don't wrap to other rows
+//            int x = tile.pos().getX();
+//            if (Math.floorDiv(x, ROW_NUMBER) != Math.floorDiv(x + tilePlacement.getTiles().size(), ROW_NUMBER)) {
+//                throw new PlacementException(String.format("'%s' is outside the board", tile.pos()), tilePlacement, Optional.of(this));
+//            }
         }
 
         // Place the tiles
@@ -88,7 +137,6 @@ public class Board {
 
     /**
      * Prints the current board state to console
-     *
      * @author Colin Mandeville, 101140289
      */
     public void printBoard() {
@@ -117,7 +165,6 @@ public class Board {
 
     /**
      * Get positioned tiles
-     *
      * @author Quinn Parrott, 101169535
      */
     public ArrayList<TilePositioned> getTiles() {
@@ -137,7 +184,6 @@ public class Board {
 
     /**
      * Get all vertical and horizontal sequences of characters on the board.
-     *
      * @author Quinn Parrott, 101169535
      */
     public HashSet<String> collectCharSequences() {
@@ -193,7 +239,6 @@ public class Board {
 
     /**
      * Clone the board instance
-     *
      * @author Quinn Parrott, 101169535
      */
     public Board clone() {
