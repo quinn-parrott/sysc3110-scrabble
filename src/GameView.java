cddd143016e4178b9bd6d9d27ddc0c35bff17fcf
Quinn Parrott <quinnparrott@cmail.carleton.ca>
2022-11-06 11:44:09 -0500

    Moved player name gui out of GameView

--- src/GameView.java
+++ src/GameView.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,7 +12,6 @@ public class GameView extends JFrame {
     private final Container pane;
     private final Game game;     // model if we'll be using MVC . Some more changes to be made in the Game class
     private final WordList wordList = new WordList();
-    private final List<Player> playersList = new ArrayList<>();
     private final JLabel playerTurnLabel;
     private final JButton playButton;
     private final JButton passTurn;
@@ -42,16 +40,15 @@ public class GameView extends JFrame {
 
         // A layout manager for these components is still pending
 
-        this.getPlayers();
+        List<Player> playersList = (new PlayerAdderView(this)).getPlayers();
+        game = new Game(playersList, wordList);
+
         pane.add(this.createBoard(), BorderLayout.WEST);
         pane.add(this.createTileHand() , BorderLayout.SOUTH);
         pane.add(this.createScoreBoard(), BorderLayout.EAST);
         this.createPlayButtons();
 
-        game = new Game(playersList, wordList);
         game.addGameView(this);
-
-
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1130, 1130);
@@ -139,79 +136,6 @@ public class GameView extends JFrame {
         button.setPreferredSize(new Dimension(1000, 100));
         grid.add(button);
         return button;
-    }
-
-    /**
-     * Method to get the number of players that will be playing the game.
-     * Player must be between 2 and 4.
-     *
-     *  @author Tao Lufula, 101164153
-     */
-    private void getPlayers() {
-        boolean gotPlayers = false;
-
-        while(!gotPlayers) {
-
-            int numberOfPlayers = 0;
-
-            JPanel playerPanel = new JPanel();
-            JTextField enterNumOfPlayers = new JTextField("Enter number of players (2 to 4) :  ");
-            enterNumOfPlayers.setEditable(false);
-            playerPanel.add(enterNumOfPlayers);
-
-            JTextField PlayersNumber = new JTextField(10);
-            playerPanel.add(PlayersNumber);
-            JOptionPane.showOptionDialog(this, playerPanel, "Players' Setup" + "                    " + "SCRABBLE ", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, null);
-            try {
-                numberOfPlayers = Integer.parseInt(PlayersNumber.getText());
-            }catch(NumberFormatException ex) {
-                JOptionPane.showMessageDialog(this, "Invalid entry! Enter number between 2 to 4");
-                continue;
-            }
-
-            if (numberOfPlayers < 2 || numberOfPlayers > 4) {
-                JOptionPane.showMessageDialog(this, "Invalid entry!  Number of players must be between 2 to 4");
-            } else {
-                getPlayersNames(numberOfPlayers);
-                gotPlayers = true;
-            }
-        }
-
-    }
-
-    /**
-     * Method to get all the players' names
-     * @param numberOfPlayers specified by the user in  getPlayers
-     *
-     *  @author Tao Lufula, 101164153
-     */
-    private void getPlayersNames(int numberOfPlayers){
-
-        for (int i = 0; i < numberOfPlayers; i++) {
-
-            Boolean validName = false;
-
-            while (!validName) {
-
-                JPanel addNamePanel = new JPanel();
-                JTextField enterName = new JTextField("Enter name of player " + (i + 1));
-                enterName.setEditable(false);
-                addNamePanel.add(enterName);
-
-                JTextField getName = new JTextField(10);
-                addNamePanel.add(getName);
-                JOptionPane.showOptionDialog(this, addNamePanel, "Players Names", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, null);
-
-                if (getName.getText().isEmpty()) {
-                    JOptionPane.showMessageDialog(this, "Invalid entry! Name cannot be empty");
-                }
-                else {
-                    playersList.add(new Player(getName.getText()));
-                    validName = true;
-                }
-
-            }
-        }
     }
 
 
