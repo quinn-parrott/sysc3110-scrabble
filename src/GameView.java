fd3e93244ca58299e96c9bd4c2c17931116c190e
Quinn Parrott <quinnparrott@cmail.carleton.ca>
2022-11-10 17:03:05 -0500

    Added basic board and hand GUI

--- src/GameView.java
+++ src/GameView.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,6 +10,9 @@ import java.util.List;
  *
  */
 public class GameView extends JFrame {
+    private static final Color colorUnselected = new Color(240, 240, 240);
+    private static final Color colorSelected = new Color(200, 200, 200);
+
     private final Container pane;
     private final Game game;     // model if we'll be using MVC . Some more changes to be made in the Game class
     private final JLabel playerTurnLabel;
@@ -32,15 +37,23 @@ public class GameView extends JFrame {
         playButton = new JButton();
         passTurn = new JButton();
 
-
-
         // A layout manager for these components is still pending
 
         List<Player> playersList = (new PlayerAdderView(this)).getPlayers();
+
+        TileBag gameBag = new TileBag();
+        int PLAYER_HAND_SIZE = 7;
+        for (Player player : playersList) {
+            for (int i = 0; i < PLAYER_HAND_SIZE; i++) {
+                player.getTileHand().add(gameBag.drawTile());
+            }
+        }
+
         game = new Game(playersList, new WordList());
 
-        pane.add(this.createBoard(), BorderLayout.WEST);
-        pane.add(this.createTileHand() , BorderLayout.SOUTH);
+
+        pane.add(this.createBoard(game.getBoard()), BorderLayout.WEST);
+        pane.add(this.createTileHand(playersList.get(0)) , BorderLayout.SOUTH);
         pane.add(this.createScoreBoard(), BorderLayout.EAST);
         this.createPlayButtons();
 
@@ -110,29 +123,73 @@ public class GameView extends JFrame {
     }
 
 
-    /**
-     * To be implemented
-     */
-    private Component createTileHand() {
-        // TODO: Implement this
-        JPanel grid = new JPanel(new GridLayout(1, 1));
-        JButton button = new JButton("Tile Hand and Play Buttons will be here!.  Jlabel to tell whose turn it is will also be here.");
-        button.setPreferredSize(new Dimension(100, 200));
-        grid.add(button);
-        return grid;
+    private Component createTileHand(Player player) {
+        var tiles = player.getTileHand();
+        JPanel gridPanel = new JPanel(new GridLayout(1, tiles.size()));
+        gridPanel.setPreferredSize(new Dimension(100, 200));
+
+        var buttons = new ArrayList<JButton>();
+
+        for (Tile tile : tiles) {
+            JButton button = new JButton(String.format("%c tile", tile.chr()));
+            button.setBackground(colorUnselected);
+            button.addActionListener(event -> {
+                var but = (JButton) event.getSource();
+
+                var isSelected = but.getBackground() == colorSelected;
+
+                // Reset all the buttons
+                for (JButton b : buttons) {
+                    b.setBackground(colorUnselected);
+                }
+
+                but.setBackground(
+                        isSelected ? colorUnselected : colorSelected
+                );
+            });
+            buttons.add(button);
+
+            gridPanel.add(button);
+        }
+
+        return gridPanel;
     }
 
 
 
-    /**
-     * To be implemented just
-     */
-    private Component createBoard() {
-        JPanel grid = new JPanel(new GridLayout(1, 1));
-        JButton button = new JButton("Game Board will be here!");
-        button.setPreferredSize(new Dimension(1000, 100));
-        grid.add(button);
-        return button;
+    private Component createBoard(Board board) {
+        JPanel grid = new JPanel(new GridLayout(Board.getROW_NUMBER(), Board.getCOLUMN_NUMBER()));
+        grid.setPreferredSize(new Dimension(1000, 100));
+
+        for (int i =0; i< (Board.getCOLUMN_NUMBER() * Board.getROW_NUMBER()); i++) {
+            var pos = Position.FromIndex(i).get();
+            var tile = board.getTile(pos).get();
+            JButton button = new JButton(String.format("%c", tile.chr()));
+
+            var isTilePlaceable = false;
+            if (!tile.isFilledWithLetter()) { // Can't place on filled tile
+                if (Board.getCenterTilePos() == pos.getIndex()) {
+                    // Center tile is a special case since it's possible to place
+                    // the tile here when there are no other tiles.
+                    isTilePlaceable = true;
+                } else {
+                    // Only make the tile placeable if there is a tile adjacent that has a letter
+                    for (var adjacentPos : pos.adjacentPositions()) {
+                        var adjacentTile = board.getTile(adjacentPos).get();
+                        if (adjacentTile.isFilledWithLetter()) {
+                            isTilePlaceable = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            button.setEnabled(isTilePlaceable);
+
+            button.setBackground(colorUnselected);
+            grid.add(button);
+        }
+
+        return grid;
     }
 
 
